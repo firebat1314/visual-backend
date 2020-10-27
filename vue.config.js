90eedf8330ec075857f31d903f8cea5e511240e8
firebat1314 <1121112856@qq.com>
2020-10-27 17:10:27 +0800

    更新模板

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "页面装修";
+      // favicon: "./public/favicon.ico",
+      // template: "./public/index.html",
+      // inject: true,
+      return args;
+    });
+  },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9093,
-  }
-}
+  },
+};
